038d3ae10753956f794c6d07dd8f6582965ed24f
unknown <s4929892@bournemouth.ac.uk>
2022-02-01 15:57:52 +0000

    ScriptableObject Logic

---
+++ The Flyweight Pattern/Assets/Scripts/PlantData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="plantData",menuName ="Plant Data",order = 51)]
+public class PlantData : ScriptableObject
+{
+    public enum THREAT { None,Low,Moderate,Hight}
+
+    [SerializeField]
+    private string plantName;
+    [SerializeField]
+    private THREAT plantThreat;
+    [SerializeField]
+    private Texture icon;
+}
